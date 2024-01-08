f532173efd98e050f7e724b896c2d5b60e2a3135
Simos Tigkas <stigkas@c1r6p16.hive.fi>
2024-01-08 16:41:39 +0200

    almost there

--- Makefile
+++ Makefile
@@ -9,14 +9,3 @@ LIBFTA = libft.a
 INC = includes
 
 CFLAGS = -Wall -Wextra -Werror
-
-%.o: %.c
-	gcc $(CFLAGS) -I$(INC) -c $< -o $@ 
-
-$(NAME): $(OBJ)
-	ar crs $(NAME) $(OBJ)
-
-all: $(NAME)
-
-clean:
-	rm -f $(OBJ) $(BONUS_OBJ)
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 10:15:05 by stigkas           #+#    #+#             */
-/*   Updated: 2024/01/08 13:19:27 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/01/08 16:31:21 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,43 @@
 
 void	handle_signal(int bit)
 {
-	static int	bit_index = 0;
-	static char	current_char = 0;
+	static int	bit_index;
+	static char	current_char;
+
+	current_char = 0;
 
 	current_char |= (bit << bit_index);
-	if (++bit_index == 8)
+	++bit_index;
+	if (bit_index == 8)
 	{
-		ft_printf("The current character is: %c", current_char);
+		ft_printf("The current character is: %c\n", current_char);
 		bit_index = 0;
-		current_char = 0;
 	}
+	else
+		current_char <<= 1;
 }
 
 void	sigusr1_handler(int sig)
 {
 	if (sig == SIGUSR1)
+	{
+		ft_printf("Received SIGUSR1\n");
 		handle_signal(1);
+	}
 }
 
 void	sigusr2_handler(int sig)
 {
 	if (sig == SIGUSR2)
+	{
+		ft_printf("Received SIGUSR2\n");
 		handle_signal(0);
+	}
 }
 
 int	main(void)
 {
 	pid_t		server_pid;
-	static int	bit_index = 0;
-	static char	current_char = 0;
 
 	server_pid = getpid();
 	ft_printf("Server PID: %d\n", server_pid);
